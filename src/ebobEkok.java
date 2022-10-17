5742781fa90f3cfc4141e4831b48cbede666965b
kml htb <kmlhtbdev@gmail.com>
2022-10-17 10:24:55 +0300

    Initial commit

---
+++ src/ebobEkok.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ebobEkok {
+    public static void main(String[] args) {
+        int a,b;
+        int i=1,k=1;
+        int ebob=1,ekok;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Birinci sayıyı giriniz: ");
+        a = input.nextInt();
+        System.out.print("İkinci sayıyı giriniz: ");
+        b = input.nextInt();
+
+        while(i<=a) {
+            if (a % i == 0 && b % i == 0) {
+                ebob = i;
+            }
+            i++;
+        }
+        System.out.println("EBOB: "+ebob);
+
+        while (k<=(a*b)){
+            if (k % a == 0 && k % b == 0) {
+                ekok = k;
+                break;
+            }
+            k++;
+        }
+        System.out.print("EKOK: "+k);
+    }
+}
